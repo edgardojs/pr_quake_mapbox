8d34a8de0eb948ab4261545355fd6ec80b9d1117
edgardojs <edgardoj.santiago@gmail.com>
2020-01-12 11:21:37 -0400

    working with p5

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+import RenderMap from './RenderMap';
+
+
+
+  
+class App extends React.Component {
+    render(){
+    return(
+    <div>
+        
+         <RenderMap/>
+    </div>
+ 
+    )
+    }}
+export default App;
